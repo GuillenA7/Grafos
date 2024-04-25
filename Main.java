b577f4f4b1f1f334b7b88948c09982410b940c8e
Adrian <119546624+GuillenA7@users.noreply.github.com>
2024-04-24 22:37:02 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,58 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //start main graph
-            Grafo ABC = new Grafo();
+        Grafo ABC = new Grafo();
     
-            Vertice A = ABC.agregarVertice("A");
-            Vertice B = ABC.agregarVertice("B");
-            Vertice C = ABC.agregarVertice("C");
+        Vertice A = ABC.agregarVertice("0");
+        Vertice B = ABC.agregarVertice("1");
+        Vertice C = ABC.agregarVertice("2");
+        Vertice D = ABC.agregarVertice("3");
+        Vertice E = ABC.agregarVertice("4");
+        Vertice F = ABC.agregarVertice("5");
     
-            ABC.agregarArista(A, B, 1);
-            ABC.agregarArista(A, C, 5);
-            ABC.agregarArista(C, B, 3);
-            ABC.print();
+        ABC.agregarArista(A, E, 4);
+        ABC.agregarArista(B, E, 3);
+        ABC.agregarArista(C, D, 1);
+        ABC.agregarArista(C, E, 2);
+        ABC.agregarArista(C, F, 3);
+    
+        imprimirMatrizDeAdyacenciaBinaria(ABC);
+    }
+
+    public static void imprimirMatrizDeAdyacenciaBinaria(Grafo grafo) {
+        ArrayList<Vertice> vertices = grafo.getVertices();
+        int numVertices = vertices.size();
+    
+        boolean[][] matrizAdyacencia = new boolean[numVertices][numVertices];
+    
+        for (int i = 0; i < numVertices; i++) {
+            Vertice verticeActual = vertices.get(i);
+            ArrayList<Arista> aristas = verticeActual.getAristas();
+    
+            System.out.print(verticeActual.getNombre() + " está conectado a: ");
+            if(aristas.isEmpty()) {
+                System.out.println("ningún vértice");
+            } else {
+                for (Arista arista : aristas) {
+                    System.out.print(arista.getFin().getNombre() + " ");
+                    int indiceFin = vertices.indexOf(arista.getFin());
+                    matrizAdyacencia[i][indiceFin] = true;
+                }
+                System.out.println();
+            }
         }
+    
+        System.out.println("Matriz de adyacencia:");
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                if (matrizAdyacencia[i][j]) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
